62a64ee67887aba3aee32c34f587ba9f373713f1
Nemeh998 <nemh998@gmail.com>
2021-08-09 22:49:07 +0300

    pook

---
+++ city-back/server.js
@@ -0,0 +1,43 @@
+'use strict';
+//////////////import express/////////////
+const express=require('express');
+const pookdata=require('./assits/Pokemon.json');
+console.log(pookdata)
+/////////server has all properities and method of express//////////
+const server=express();
+
+const PORT=3002;
+server.get('/test',(req,res)=>{
+    res.status(200).send('its WORK')
+})
+////////////////////////////get data from file json////////////////////
+//localhost:3002/pookdata?pookname=chesto
+//{ pookname: 'chesto' }the result
+//   console.log(req.query)
+server.get('/pookdata',(req,res )=>{
+    console.log(req.query)
+    const selectdata=pookdata.results.find(poke=>{
+        console.log(poke,'poke')
+        console.log(poke.name,'poke name')
+        if(poke.name == req.query.pookname){
+            return poke
+        }
+    })
+    res.send(selectdata);
+})
+
+/////////////////////////////////////////////
+
+server.get('/',(req,res)=>{
+    res.status(200).send('home routs')
+})
+const shoplist=['water','checken','icecreem'];
+server.get('/sopelist',(req,res)=>{
+    res.status(200).send(shoplist);
+})
+server.get('*',(req,res)=>{
+    res.status(404).send('NOT FOUND')
+})
+server.listen(PORT,()=>{
+    console.log(`listing on PORT ${PORT}`)
+})
